d9f89efdf6e2d174b7e0b19badee69ded45c6c70
Dalgom <63985491+djkdr@users.noreply.github.com>
2023-07-24 11:19:57 +0900

    Set up

---
+++ ch01_practice.py
@@ -0,0 +1,31 @@
+# 숫자 자료형
+print(5)
+print(-10)
+print(3.14)
+print(2000)
+print(5+10)
+
+# 문자열 자료형
+print("ㅋ"*9)
+
+# 참 / 거짓
+print(5>10)
+print(True)
+print(not True)
+
+# 변수
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " + name + "예요")
+print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+
+# 한 줄만
+
+''' 이렇게
+한번에 주석 처리
+된다잉 '''
