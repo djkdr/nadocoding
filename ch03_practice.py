caa18de0b4b4f2d74d16f0ab4d11616c6f086fa1
Dalgom <63985491+djkdr@users.noreply.github.com>
2023-07-25 11:02:14 +0900

    Python Study 230725

---
+++ ch03_practice.py
@@ -0,0 +1,21 @@
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3)
+print(5%3)
+print(10//3)
+
+print(10 > 3)
+print(4 >= 7)
+
+print(1 != 3)
+print(not(1 != 3))
+
+print((3 > 0) and (3 < 5))
+print((3 > 0) & (3 < 5))
+
+print((3 > 0) and (3 < 5))
+print((3 > 0) | (3 < 5))
+
